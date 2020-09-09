21e60ec3e1f68b11bb29de2d931c5e4c8cf681cb
Sergei Zakharov <sergei86zahar@mail.ru>
2020-09-09 23:15:39 +0700

    Создал папку

---
+++ Hello World!/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Learning_C_Sharp
+{
+    class HelloWorld
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+            Console.WriteLine ("Hello World twice");
+        }
+    }
+}
