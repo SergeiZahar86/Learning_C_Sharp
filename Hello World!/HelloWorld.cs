ac5e5c3127cf47e4f292c53299dc987ab58e5fd8
SergeiZahar86 <sergei86zahar@mail.ru>
2020-09-11 22:05:29 +0700

    Настроил git

--- Hello World!/HelloWorld.cs
+++ Hello World!/HelloWorld.cs
@@ -8,7 +8,7 @@ namespace Learning_C_Sharp
         {
             Console.WriteLine ("Hello World!");
             Console.WriteLine ("Hello World twice");
-            //Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
