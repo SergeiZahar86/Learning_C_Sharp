e04a5383768b0880a3ed31af31c06f3bc88ddf3e
Сергей Захаров <sergei86zahar@mail.ru>
2020-09-25 11:36:57 +0700

    одиночка

--- Hello World!/HelloWorld.cs
+++ Hello World!/HelloWorld.cs
@@ -2,13 +2,45 @@
 
 namespace Learning_C_Sharp
 {
-    class HelloWorld
+    class Program  
     {
-        static void Main (string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine ("Hello World!");
-            Console.WriteLine ("Hello World twice");
-            Console.WriteLine();
+            Computer comp = new Computer();
+            comp.Launch("Windows 8.1");
+            Console.WriteLine(comp.Os.Name);
+
+            // у нас не получится изменить ОС, так как объект уже создан    
+            comp.Os = OS.getInstance("Windows 10");
+            Console.WriteLine(comp.Os.Name);
+
+            Console.ReadLine();
+        }
+    }
+    class Computer
+    {
+        public OS Os { get; set; }
+        public void Launch(string osName)
+        {
+            Os = OS.getInstance(osName);
+        }
+    }
+    class OS
+    {
+        private static OS instance;
+
+        public string Name { get; private set; }
+
+        protected OS(string name)
+        {
+            this.Name = name;
+        }
+
+        public static OS getInstance(string name)
+        {
+            if (instance == null)
+                instance = new OS(name);
+            return instance;
         }
     }
 }
